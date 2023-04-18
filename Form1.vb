9db78367e08b7714bc00215233f9f7467f7d6777
KM <k_miyata@b-rains.co.jp>
2023-04-18 15:10:00 +0900

    [Add] Form1.vb

--- Form1.vb
+++ Form1.vb
@@ -39,9 +39,9 @@ Public Class Form1
 
         End While
 
-        console()
+        Console()
 
-        'ctl()
+        Ctl()
         conn.Close()
 
         'End Using
@@ -53,9 +53,10 @@ Public Class Form1
 
     End Sub
 
-    Private Sub ctl()
+    Private Sub Ctl()
 
-        Dim filename = "C:\Users\User\source\repos\sample1\sample1\test1.xlsx"
+        Dim filename = "C:\Users\User\test1.xlsx"
+        'Dim filename = "C:\Users\User\source\repos\sample1\sample1\test1.xlsx"
 
         Dim ea As Excel.Application = New Excel.Application
         Dim wbs As Excel.Workbooks = ea.Workbooks
@@ -65,20 +66,56 @@ Public Class Form1
         Dim ws As Excel.Worksheet = ss(1)
         Dim cs As Excel.Range = ws.Cells
 
+        'セルに入力（セル指定）
+        cs.Range("A1").Value = "hello"
+
+        'セルに入力（座標指定）
+        cs.Cells(2, 1) = "world"
+
+        '確認メッセージ非表示
+        ea.DisplayAlerts = False
+
+        '上書き保存
+        wb.Save()
+
+        '名前を付けて保存
+        'wb.SaveAs2("C:\Users\User\test2.xlsx")
+
+        '保存しないで閉じる
+        'wb.Close(False)
+
+        '保存して閉じる（上書き）
+        'wb.Close(True)
+
+        '解放
         System.Runtime.InteropServices.Marshal.ReleaseComObject(cs)
         System.Runtime.InteropServices.Marshal.ReleaseComObject(ws)
         System.Runtime.InteropServices.Marshal.ReleaseComObject(ss)
         System.Runtime.InteropServices.Marshal.ReleaseComObject(wb)
         System.Runtime.InteropServices.Marshal.ReleaseComObject(wbs)
 
+        '終了
+        ea.Quit()
 
+        '解放
         System.Runtime.InteropServices.Marshal.ReleaseComObject(ea)
 
     End Sub
 
-    Private Sub console()
+    Private Sub Console()
 
         MsgBox("OK")
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Dim f2 As Form2 = New Form2
+        Try
+            f2.ShowDialog()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
 
     End Sub
 End Class
